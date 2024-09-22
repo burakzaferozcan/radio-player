1c3fff5c3a35155a9f823fbed7d10612fa3d18d1
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-09-22 23:40:32 +0300

    favourite page created.

--- radio-player-frontend/src/routes/AppRouter.jsx
+++ radio-player-frontend/src/routes/AppRouter.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
+import Favourite from "../pages/Favourite";
 
 function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/favourite" element={<Favourite />} />
       <Route path="*" element={<Navigate to={"/"} />} />
     </Routes>
   );
