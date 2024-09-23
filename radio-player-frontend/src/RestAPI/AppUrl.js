13c8e8cc5a5b2ace7a013a340e6420fd0b814240
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-09-23 11:22:25 +0300

    RestClient dosyası oluşturuldu, bildirim ayarları tamamlandı, AppUrl tanımlandı..

---
+++ radio-player-frontend/src/RestAPI/AppUrl.js
@@ -0,0 +1,6 @@
+class AppUrl {
+  static baseURL = "http://localhost:8000";
+  static apiURL = "http://localhost:8000/api";
+}
+
+export default AppUrl;
---
+++ radio-player-frontend/src/RestAPI/Notification.js
@@ -0,0 +1,22 @@
+import Swal from "sweetalert2";
+
+class Notification {
+  static success = (data) => {
+    return Swal.fire({
+      title: data.title,
+      text: data.text,
+      icon: "success",
+      confirmButtonText: "OK",
+    });
+  };
+
+  static error = (data) => {
+    return Swal.fire({
+      title: data.title,
+      text: data.text,
+      icon: "error",
+      confirmButtonText: "OK",
+    });
+  };
+}
+export default Notification;
