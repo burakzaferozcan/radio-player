a99cf9c1b468e802d49d5ec8e7988d80f0d8b89a
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-09-23 11:44:22 +0300

    Favourite model & migration created.

--- radio-player-backend/app/Models/FavouriteModel.php
+++ radio-player-backend/app/Models/FavouriteModel.php
@@ -6,14 +6,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class RadioModel extends Model
+class FavouriteModel extends Model
 {
     use HasFactory;
     use SoftDeletes;
-    protected $table = "radios";
-    protected $primaryKey="rd_id";
+    protected $table = "favourites";
+    protected $primaryKey="fw_id";
     protected $guarded=[];
-    public const CREATED_AT="rd_created_at";
-    public const UPDATED_AT="rd_updated_at";
-
+    public const CREATED_AT="fw_created_at";
+    public const UPDATED_AT="fw_updated_at";
 }
