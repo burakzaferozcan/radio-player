bfc2ce7e5427b6651ca79cc020f6aedb4c1d8db8
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-09-24 19:06:58 +0300

    feature: backend - radio list HomeController created.

---
+++ radio-player-backend/app/Http/Controllers/api/home/indexController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\api\home;
+
+use App\Http\Controllers\api\BaseController;
+use App\Models\RadioModel;
+use Illuminate\Http\Request;
+
+class indexController extends BaseController
+{
+    //
+    public function index(Request $request){
+        $client = $request->user();
+        $radio=RadioModel::all();
+
+        return parent::success("Radyo listesi getirildi",$radio);
+    }
+}
