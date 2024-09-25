be0b7a779b20a2146b0603ac46c916fa72981d28
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-09-25 12:39:34 +0300

    feature: create remove_favourite function to delete favourites in the backend.

--- radio-player-backend/app/Http/Controllers/api/favourite/indexController.php
+++ radio-player-backend/app/Http/Controllers/api/favourite/indexController.php
@@ -14,4 +14,22 @@ class indexController extends BaseController
         ->where("favourites.fw_user",$client->id)->get();
         return parent::success("Favori listesi getirildi",$radios);
     }
+
+    public function remove_favourite(Request $request)
+    {
+        $client = $request->user();
+        $data = $request->except("_token");
+
+        $control = FavouriteModel::where([
+            ["fw_user","=",$client->id],
+            ["fw_radio","=",$data['fw_radio']],
+        ])->first();
+
+        if ($control){
+            FavouriteModel::where([
+                ["fw_user","=",$client->id],
+                ["fw_radio","=",$data['fw_radio']],
+            ])->delete();
+        }
+    }
 }
